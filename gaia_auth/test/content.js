85e57e6ee328c758d329acedf2ce6165567ce517
Zelidrag Hornung <zelidrag@google.com>
2011-08-30 20:54:49 -0700

    Added support for passing through test user email/password for test automation.
    TEST=none
    BUG=chromium-os:19866
    
    Change-Id: Ia87a4225a5b33930083cd5d1f18b3df89994a600
    Reviewed-on: http://gerrit.chromium.org/gerrit/6976
    Reviewed-by: Zelidrag Hornung <zelidrag@chromium.org>
    Tested-by: Zelidrag Hornung <zelidrag@chromium.org>

---
+++ gaia_auth/test/content.js
@@ -0,0 +1,28 @@
+// Copyright (c) 2011 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+console.log('Test script injected!');
+
+function getQueryParam(key, defaultVal) {
+  if (!defaultVal) defaultVal = "";
+  key = key.replace(/[\[]/,"\\\[").replace(/[\]]/, "\\\]");
+  var regex = new RegExp("[\\?&]" + key + "=([^&#]*)");
+  var qs = regex.exec(window.location.href);
+  if (qs == null)
+    return defaultVal;
+  else
+    return qs[1];
+}
+
+if (document.URL.match(/https\:\/\/www\.google\.com\/accounts\/ServiceLogin/) ||
+    document.URL.match(/https\:\/\/accounts\.google\.com\/ServiceLogin/)) {
+  var testEmail = getQueryParam('test_email');
+  var testPassword = getQueryParam('test_pwd');
+  console.log('Got test account info: ' + testEmail + '/' + testPassword);
+  document.getElementById('Email').value = unescape(testEmail);
+  document.getElementById('Passwd').value = unescape(testPassword);
+  console.log('Form field changed!');
+  document.getElementById('gaia_loginform').submit();
+  console.log('Form submitted!');
+}
