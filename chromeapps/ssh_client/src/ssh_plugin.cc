906913615e28628ce0289d01eb4f87b5fab97e2a
David Benjamin <davidben@mit.edu>
2012-07-14 14:44:19 -0400

    Add some hack to redirect stdout in the SSH plugin
    JavaScript side isn't hooked up yet. Really I want to control this from
    JavaScript, but this'll do for now.

--- chromeapps/ssh_client/src/ssh_plugin.cc
+++ chromeapps/ssh_client/src/ssh_plugin.cc
@@ -27,6 +27,27 @@ SshPluginInstance::~SshPluginInstance() {
 }
 
 void SshPluginInstance::SessionThreadImpl() {
+  // Replace stdout with a pipe to the outside shell.
+  //
+  // TODO(davidben): Make the Javacript <-> NaCl interface not a hack
+  // so we needn't hard-code this into the binary.
+  int out = open("/dev/js/ssh_mosh_pipe", O_WRONLY);
+  if (out < 0) {
+    perror("open");
+    exit(1);
+  }
+  // HACK: Standard FDs are special-cased so keep a reference around
+  // to the original to avoid closing it and blowing up.
+  if (dup(STDOUT_FILENO) < 0) {
+    perror("dup");
+    exit(1);
+  }
+  if (dup2(out, STDOUT_FILENO) < 0) {
+    perror("dup2");
+    exit(1);
+  }
+  close(out);
+
   // Call renamed ssh main.
   std::vector<const char*> argv;
   // argv[0]
